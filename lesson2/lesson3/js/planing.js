958be7eb7e1fbdf8d3e6ff197e3ce42b26242344
nunthitafuno <sam21001@byui.edu>
2021-10-02 00:37:51 +0700

    update

--- lesson2/lesson3/js/planing.js
+++
@@ -1,4 +0,0 @@
-const lastvisiting = document.lastModified;
-document.getElementById("outcome").textContent = "Last Updated: " + lastvisiting;
-const currentYear = new Date();
-document.getElementById("Year").textContent = currentYear.getFullYear();
