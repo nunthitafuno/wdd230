869764e28ea6bf0cb79b56de613a4104b349a152
nunthitafuno <sam21001@byui.edu>
2021-10-29 02:38:25 +0700

    code update

--- lesson7gallery/js/time.js
+++ lesson7gallery/js/time.js
@@ -1,38 +1,3 @@
-document.addEventListener ("onclick",function() {
-    // get the name from local storage
-    let usersName = localStorage.getItem('uName');
-        // calls and passes name to banner function
-        welcomeBanner(usersName);
- }, false);
-
-function welcomeBanner(name) {
-   
-    let greetingArray = [
-        'We\'re so glad you\'re here.',
-        'Have a wonderful day!',
- ];
-
-    let b = document.getElementsByClassName('welcome-banner')[0];
-  
-    if (name != null) {
-      
-        let getNum = Math.floor(Math.random() * greetingArray.length);
-     
-        b.textContent = `Hey ${name}, ${greetingArray[getNum]}! It's been ${compareDateTime()} days since your last visit.`;
-       
-        b.classList.remove('hidden');
-    } else {
-     
-        b.textContent = `Hello, it's been ${compareDateTime()} days since your last visiting.`;
-    
-        b.classList.remove('hidden');
-}
-    }
-
-function addDateLocal() {
-    let dateTime = new Date();
-    localStorage.setItem('lastVisitDate', dateTime);
-}
 
 function compareDateTime() {
     let storedDate = localStorage.getItem('lastVisitDate');
@@ -53,3 +18,12 @@ function compareDateTime() {
         return 0;   
     }
 }
+
+if ('lastVisit' in localStorage) {
+    document.querySelector('#daysSinceVisit').innerHTML = `Welcome Back! It has been ${daysSinceVisit.toFixed(0)} days since your last visit`;
+} else {
+    document.querySelector('#daysSinceVisit').innerHTML = `It looks like this is your first visit to the gallery. Welcome to the Whether Weather site!`;
+}
+
+
+
