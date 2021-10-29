331aaa11f51c23be8ff89c6ba5299f2b6622bb2e
nunthitafuno <sam21001@byui.edu>
2021-10-29 08:54:27 +0700

    update

--- lesson7gallery/js/time.js
+++ lesson7gallery/js/time.js
@@ -19,8 +19,8 @@ function compareDateTime() {
 }
 
 if ('lastVisit' in localStorage) {
-    document.querySelector('#diff').innerHTML = `Welcome Back! It has been ${daysSinceVisit.toFixed(0)} days since your last visit`;
+    document.querySelector('#diff').innerHTML = `Welcome Back! It has been days since your last visit`;
 } else {
-    document.querySelector('#diff').innerHTML = `It seems like this is your first visiing to the Whether Weather site. Welcome to Gallery Page!`;
+    document.querySelector('#diff').innerHTML = `It seems like this is your first visiting to the Whether Weather site. Welcome to Gallery Page!`;
 }
 
