4d2351d7bc36f90876589783c8bad4c056097ed1
nunthitafuno <sam21001@byui.edu>
2021-10-29 23:45:29 +0700

    update

--- lesson7gallery/js/lastvisit.js
+++ lesson7gallery/js/lastvisit.js
@@ -1,14 +1,39 @@
-const getDate = new Date()
-const dayOpition = {
-    day: "numeric",
-  };
-  const dayVisit = getDate.toLocaleDateString(undefined, dayOpition);
-localStorage.setItem('dateLastvisit',dayVisit)
+function addDateLocal() {
+  // creates var for current date/time
+  let dateTime = new Date();
+  // adds visit time to local storage
+  localStorage.setItem('lastVisit', dateTime);
+}
 
-sessionStorage.setItem('dateLastvisit',dayVisit)
+// compares the stored and current date
+function compareDateTime() {
+  // gets value of stored date
+  let storedDate = localStorage.getItem('lastVisit');
 
-const firstvisit = localStorage.getItem('dateLastvisit')
-const lastvisit = sessionStorage.getItem('dateLastvisit')
-const different = firstvisit - lastvisit;
+  // ** this is here to test functionality of past dates ** //
+  // storedDate = new Date('06/03/2020');
 
-document.getElementById('lastVisitdate').innerHTML = "Hey! It has been " + different + " days since you last visit the Gallery.";
+  if (storedDate != null) {
+      // convert stored date to an active date
+      let convertedStoredDate = new Date(storedDate);
+      // gets current date / time
+      let currentDate = new Date();
+      // gets difference between old and new dates
+      let diff = currentDate.getTime() - convertedStoredDate.getTime();
+      // reset the local (last visit) date
+      addDateLocal();
+      // returns number of days difference, rounded down
+      return Math.floor(diff / (1000 * 3600 * 24));
+  } else {
+      // set the local (last visit) date
+      addDateLocal();
+      return 0;   
+  }
+}
+
+if ('lastVisit' in localStorage) {
+  document.querySelector('#daysSinceVisit').innerHTML = `Welcome Back! It has been ${daysSinceVisit.toFixed(0)} days since your last visit`;
+} else {
+  document.querySelector('#daysSinceVisit').innerHTML = `It looks like this is your first visit to the gallery. Welcome to the site!`;
+}
+
