adad57659ae0f71e2377c83b04db0ee40b36ea7a
nunthitafuno <sam21001@byui.edu>
2021-11-09 04:45:52 +0700

    update

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -2,34 +2,30 @@ const requestURL = 'https://byui-cit230.github.io/canvas-referenced/latter-day-p
 fetch(requestURL)
   .then(function (response) {
     return response.json();
-})
- .then(function (jsonObject) {
-     const prophets = jsonObject['prophets'];
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); 
     
+    const prophets = jsonObject['prophets'];
     
-     for (let i = 0; i < prophets.length; i++ ) {
-     
+    for (let i = 0; i < prophets.length; i++ ) {
+        console.log(prophets[i]);
         let card = document.createElement('area');
         let h2 = document.createElement('h2');
-        let birth = document.createElement('span');
-        let place = document.createElement('span');
-        let image = document.createElement('img');
-          //use templet Literals
+        let birth = document.createElement('p');
+        let place = document.createElement('p');
+        let img = document.createElement('img');
+        
         h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
         birth.textContent = `Date of Birth: ${prophets[i].birthdate}`;
         place.textContent = `Place of Birth: ${prophets[i].birthplace}`;
-        image.src = prophets[i].imageurl;
-        image.alt = `${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}`;
+        img.src = prophets[i].imageurl;
+        img.alt = `${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}`;
 
-        card.appendChild(h2);
-        card.appendChild(birth);
-        card.appendChild(place);
-        card.appendChild(image);
-       
+        card.appendChild(h2).appendChild(birth).appendChild(place).appendChild(img);
         document.querySelector('div.cards').appendChild(card);
-     }
- });
-
+    }
+});
 
 
 
