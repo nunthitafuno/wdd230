25bc67fb60a9775527adbe0befedb385ad7cc042
nunthitafuno <sam21001@byui.edu>
2021-11-09 12:49:51 +0700

    update code

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,5 +1,4 @@
-const requestURL = 'https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
-
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 fetch(requestURL)
   .then(function (response) {
     return response.json();
