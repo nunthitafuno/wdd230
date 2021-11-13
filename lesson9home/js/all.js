2df7e788b6dc4122d2f9a30a008230912d14616f
nunthitafuno <sam21001@byui.edu>
2021-11-13 23:52:36 +0700

    code update

--- lesson9home/js/all.js
+++ lesson9home/js/all.js
@@ -13,4 +13,4 @@ var currentDate = new Date();
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-document.getElementById("output").innerHTML = days[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " " + currentDate.getFullYear();
+document.getElementById("outcome").innerHTML = days[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " " + currentDate.getFullYear();
